9eaeda638946bac7bbd4115d54ba634d67c840f9
Cal Farrell Woods <153258375+Cal-Woods@users.noreply.github.com>
2025-01-21 00:40:24 +0000

    Added full NextJS Dashboard project files.

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <div className="h-full">
+            <p className="text-center">Dashboard Page</p>
+            <button type="button" className="w-1/2 block bg-slate-300 m-auto mt-4 rounded-2xl">Click bitch!</button>
+        </div>
+
+)
+}
