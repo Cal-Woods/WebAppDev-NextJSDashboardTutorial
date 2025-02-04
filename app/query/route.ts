1d35093783045cb1da45036289610e5200657063
Cal Woods <D00270955@student.dkit.ie>
2025-02-04 09:38:34 +0000

    Database ready.

---
+++ app/query/route.ts
@@ -0,0 +1,26 @@
+import { db } from '@vercel/postgres';
+
+const client = await db.connect();
+
+async function insertCustomerData() {
+    await client.sql `
+    INSERT INTO customers (name, email, image_url) VALUES (
+        'Cal Farrell', 'calfarrell7785@gmail.com', 'www.kissmyass.com'
+    )
+    on conflict(id) DO NOTHING;
+    `;
+}
+
+export async function GET() {
+    try {
+        await client.sql`BEGIN`;
+        await insertCustomerData();
+        await client.sql`COMMIT`;
+
+        return Response.json("Customer commit successful");
+    }
+    catch(error) {
+        await client.sql`ROLLBACK`;
+        return Response.json({error});
+    }
+}
