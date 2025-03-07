1250dbcdd20e4c7883eadbd62312830faf6fea79
Cal Woods <D00270955@student.dkit.ie>
2025-03-07 10:59:24 +0000

    Structure: Moved loading.tsx into a new folder/route, app/dashboard/(overview).
    Added a route group, a folder wrapped in parenthesis', to separate loading.tsx from dashboard sub-routes.
    
    Route groups allow only the folder directly above to be affected by layouts/loading files.

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,5 +1,5 @@
 //Import DashboardSkeletons from 'app/ui/skeletons'
-import DashboardSkeleton from "../ui/skeletons";
+import DashboardSkeleton from "../../ui/skeletons";
 
 export default function Loading() {
     return (
